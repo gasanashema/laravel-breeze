5fb7842de1cc1c07b52dd87fc4ebef5834403a0b
Shema <shemaphilbert8@gmail.com>
2025-04-10 14:48:06 +0200

    View data and Route wildcards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,4 @@
 <x-slot:heading>Home Page</x-slot:heading>
 
 
-    @foreach($jobs as $job)
-
-        <li><strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year.</li>
-
-    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,53 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home',[
+    return view('home');
+});
+// about route
+Route::get('/jobs', function () {
+    return view('jobs',[
         'jobs' => [
             [
+                'id' => 1,
                 'title' => 'Director',
                 'salary' => '$50,000'
             ],
             [
+                'id' => 2,
                 'title' => 'Manager',
                 'salary' => '$40,000'
             ],
             [
+                'id' => 3,
                 'title' => 'Engineer',
                 'salary' => '$30,000'
             ]
         ]
     ]);
 });
-// about route
-Route::get('/about', function () {
-    return view('about');
+// single job route
+Route::get('/jobs/{id}', function ($id) {
+   $jobs = [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Manager',
+                'salary' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Engineer',
+                'salary' => '$30,000'
+            ]
+            ];
+
+            $job =\Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+            return view('job',['job'=>$job]);
 });
 // contact route
 Route::get('/contact', function () {
